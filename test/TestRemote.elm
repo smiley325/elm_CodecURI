be667a0721e2f9238dac30946be80032d5771998
Gabriel Riba Faura <griba2001@gmail.com>
2014-03-18 17:13:44 +0100

    added test/TestRemote.elm

---
+++ test/TestRemote.elm
@@ -0,0 +1,34 @@
+module TestRemote where
+
+import Graphics.Input as Input
+import open Http 
+import String (trim)
+import JavaScript as JS
+import JavaScript (JSString)
+import CURI_Data.CodecURI as C
+
+(fld1Elem, fld1Status) = Input.field "enter numeric"
+
+url = "http://localhost:3000/echo-status"
+
+urlSignal = lift (\str -> url ++ "; query status=" ++ str) fld1Status
+
+-- local echo
+fld2Elem = lift plainText urlSignal
+
+myGet url status = C.urlEncodedGet url [("status",status)] [] False
+
+myPost url status = C.urlEncodedPost url [("status",status)] [] False
+
+resultSignal = send <| lift (myPost url) fld1Status 
+
+showResult : Response String -> String
+showResult result = case result of
+                      Waiting -> "waiting"
+                      Success str -> "success: " ++ trim str
+                      Failure iStatus msg -> "failure! " ++ show iStatus ++ msg
+
+--remote echo from localhost server                     
+fld3Elem = lift (plainText . showResult) resultSignal
+
+main = lift (flow down) <| combine [fld1Elem, fld2Elem, fld3Elem]
